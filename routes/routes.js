c24d447dd37913af98581c2e6dfd3234ba3647e0
Jacquelyn Yuen <yuencd@gmail.com>
2016-10-09 19:16:30 +0800

    categories are appearing

--- routes/routes.js
+++ routes/routes.js
@@ -8,12 +8,22 @@ module.exports = function(app, passport){
       if(err){
         res.send(err);
       };
-        var itemChunks = [];
-        var chunkSize = 3;
-        for(var i = 0; i < items.length; i += chunkSize){
-          itemChunks.push(items.slice(i, i + chunkSize));
-        };
-        res.render('shop/menu', {inventory : itemChunks});
+      var itemChunks = [];
+      var chunkSize = 3;
+      for(var i = 0; i < items.length; i += chunkSize){
+        itemChunks.push(items.slice(i, i + chunkSize));
+      };
+      res.render('shop/menu', {inventory : itemChunks});
+    });
+  });
+
+ app.get('/categories', function(req,res, next){ //what is next for?
+
+    Inventory.distinct('category', function(err, categories){
+      if(err){
+        res.send(err);
+      };
+      res.render('partials/menu_filter_nav_bar.pug', {category : categories});
     });
   });
 
