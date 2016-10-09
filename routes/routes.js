a5c6ed48a83d61a47873f9bf9b67693189c15bb7
Jacquelyn Yuen <yuencd@gmail.com>
2016-10-09 20:45:20 +0800

    two queries in one app.get(express fx)

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,8 @@ const Inventory = require('../models/inventory');
 
 module.exports = function(app, passport){
 
-  app.get('/', function(req,res, next){ //what is next for?
-
-    Inventory.find(function(err, items){
+  function findInventoryDatabase(req, res, next) {
+      Inventory.find(function(err, items){
       if(err){
         res.send(err);
       };
@@ -13,19 +12,63 @@ module.exports = function(app, passport){
       for(var i = 0; i < items.length; i += chunkSize){
         itemChunks.push(items.slice(i, i + chunkSize));
       };
-      res.render('shop/menu', {inventory : itemChunks});
+      req.inventory = itemChunks;
+      next();
     });
-  });
+  };
 
- app.get('/categories', function(req,res, next){ //what is next for?
-
-    Inventory.distinct('category', function(err, categories){
+  function distinctInventoryCategory(req, res, next){
+    Inventory.distinct('category', function(err, items){
       if(err){
         res.send(err);
       };
-      res.render('partials/menu_filter_nav_bar.pug', {category : categories});
+      req.category = items;
+      next()
     });
-  });
+  };
+
+  function renderMenu(req, res){
+    res.render('shop/menu', {
+        inventory: req.inventory,
+        category: req.category
+    });
+  };
+
+  app.get('/', findInventoryDatabase, distinctInventoryCategory, renderMenu);
+
+
+  // function(req,res, next){ //what is next for?
+
+  //   Inventory.find(function(err, items){
+  //     if(err){
+  //       res.send(err);
+  //     };
+  //     var itemChunks = [];
+  //     var chunkSize = 3;
+  //     for(var i = 0; i < items.length; i += chunkSize){
+  //       itemChunks.push(items.slice(i, i + chunkSize));
+  //     };
+
+  //     res.render('shop/menu', {inventory : itemChunks});
+  //   });
+  // }, function(req, res, next){
+  //   Inventory.distinct('categories', function(err, categories){
+  //     if(err){
+  //       res.send(err)
+  //     };
+  //     res.render('shop/menu', {category : categories});
+  //     });
+  // });
+
+  // app.get('/categories', function(req,res, next){ //what is next for?
+
+  //   Inventory.distinct('category', function(err, categories){
+  //     if(err){
+  //       res.send(err);
+  //     };
+  //     res.render('partials/menu_filter_nav_bar.pug', {category : categories});
+  //   });
+  // });
 
 
 // Do Not Delete This Curly Bracket.
