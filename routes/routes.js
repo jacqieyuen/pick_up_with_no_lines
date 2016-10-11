7fbd291d679cc3823bf326a6e01f0cbfcf026fd8
Jacquelyn Yuen <yuencd@gmail.com>
2016-10-11 11:48:44 +0800

    show by category

--- routes/routes.js
+++ routes/routes.js
@@ -7,12 +7,12 @@ module.exports = function(app, passport){
       if(err){
         res.send(err);
       };
-      var itemChunks = [];
-      var chunkSize = 3;
-      for(var i = 0; i < items.length; i += chunkSize){
-        itemChunks.push(items.slice(i, i + chunkSize));
-      };
-      req.inventory = itemChunks;
+      // var itemChunks = [];
+      // var chunkSize = 3;
+      // for(var i = 0; i < items.length; i += chunkSize){
+      //   itemChunks.push(items.slice(i, i + chunkSize));
+      // };
+      req.inventory = items;
       next();
     });
   };
@@ -35,40 +35,6 @@ module.exports = function(app, passport){
   };
 
   app.get('/', findInventoryDatabase, distinctInventoryCategory, renderMenu);
-
-
-  // function(req,res, next){ //what is next for?
-
-  //   Inventory.find(function(err, items){
-  //     if(err){
-  //       res.send(err);
-  //     };
-  //     var itemChunks = [];
-  //     var chunkSize = 3;
-  //     for(var i = 0; i < items.length; i += chunkSize){
-  //       itemChunks.push(items.slice(i, i + chunkSize));
-  //     };
-
-  //     res.render('shop/menu', {inventory : itemChunks});
-  //   });
-  // }, function(req, res, next){
-  //   Inventory.distinct('categories', function(err, categories){
-  //     if(err){
-  //       res.send(err)
-  //     };
-  //     res.render('shop/menu', {category : categories});
-  //     });
-  // });
-
-  // app.get('/categories', function(req,res, next){ //what is next for?
-
-  //   Inventory.distinct('category', function(err, categories){
-  //     if(err){
-  //       res.send(err);
-  //     };
-  //     res.render('partials/menu_filter_nav_bar.pug', {category : categories});
-  //   });
-  // });
 
 
 // Do Not Delete This Curly Bracket.
